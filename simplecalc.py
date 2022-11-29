db6b036de6ad46e8ba276930bf6d82aaf3dfbac6
georgegathu <ggeorge.gathu@gmail.com>
2022-11-29 12:00:56 +0300

    Calc for adding two values

---
+++ simplecalc.py
@@ -0,0 +1,8 @@
+# Add two numbers and display the sum
+
+first_number = input('First number: ')
+second_number = input('Second number: ')
+
+sum = float(first_number) + float(second_number)
+
+print(sum)
