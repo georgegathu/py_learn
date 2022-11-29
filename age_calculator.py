bdb22cb4f01040cc4809e5adb48d2178f0201d23
georgegathu <ggeorge.gathu@gmail.com>
2022-11-29 11:19:56 +0300

    Type conversion from string to integer

---
+++ age_calculator.py
@@ -0,0 +1,11 @@
+"""
+    -A program to Calculate current age
+    -We shall use type conversion where birth_year is a string and we convert 
+     it into an integer
+
+"""
+
+birth_year = input('Year of birth: ')
+age = 2022 - int(birth_year)
+
+print(age)
