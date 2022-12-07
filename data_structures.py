763ce9341f88f3ba02974df0aefec930ba8174b2
georgegathu <ggeorge.gathu@gmail.com>
2022-12-07 13:52:18 +0300

    Strings in Data Structure

---
+++ data_structures.py
@@ -0,0 +1,17 @@
+#PYTHON STRING
+"""
+    A string is a sequence of characters that can be a combination of 
+    letters, numbers, and special characters.
+    It can be declared by using single quotes, double quotes, or even triple quotes.
+    These quotes are not a part of a string, they define only starting and ending of the string
+    Strings are immutable, i.e., they cannot be changed.
+    Each element of the string can be accessed using indexing or slicing operations.
+"""
+a = '"A"Is the first alphabet letter'
+print(a)
+b = "Be your own boss 24/7"
+print(b)
+c = '''CATS must be pased '''
+print(c)
+
+
