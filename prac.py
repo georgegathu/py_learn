5ae173a220bdb1c7b4eac76161a07ce16d15a13d
georgegathu <ggeorge.gathu@gmail.com>
2022-12-28 19:41:21 +0300

    A program that prints Multiple strings

--- prac.py
+++ prac.py
@@ -1,8 +1,14 @@
-#TYPE CONVERSION 
+#STRINGS 
 
-# Write a program that asks a user their weight(In Pounds) and convert it into Kgs 
-# print out weight in KGs
-weight_in_pounds = input("Input Weight in Pounds: ")
-weight_in_kgs = int(weight_in_pounds) * 0.45
+# Write a program with multiple strings (Email format)
 
-print(weight_in_kgs)
+course = '''
+Hi Blacks,
+
+Hope this email finds you well,
+We are pleased to inform you , you have passed your interview.
+CONGRATS!
+
+ICT DEPARTMENT
+'''
+print(course)
