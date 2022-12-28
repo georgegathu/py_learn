1de20865be444246b6add90a72b18eaf1877795a
georgegathu <ggeorge.gathu@gmail.com>
2022-12-28 21:31:41 +0300

    String Methods

--- prac.py
+++ prac.py
@@ -1,7 +1,9 @@
-#FORMATED STRINGS 
+#STRING METHODS
+food = 'Eat to Live dont Live to Eat'
 
-model = 'Toyota Tundra'
-model_year = '2020'
-msg = f'{model} [{model_year}] is an exeptional car'
-
-print(msg)
+print(len(food)) #len() - Count the numbers of characters in a string 
+print(food.upper()) #upper() - Convert a string to upper case
+print(food.lower()) #lower() - Convert a string to lower case
+print(food.find('Eat')) #find() - returns the index of a character in string
+print(food.replace('Eat', 'Munch')) #replace() - replace charaters and words in a string
+print('Eat' in food) #Boolean expression
