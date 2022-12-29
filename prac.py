3d50ce60475c945e8b8d9d48b53bc3b916b95c2e
georgegathu <ggeorge.gathu@gmail.com>
2022-12-29 16:47:24 +0300

    Augmented arithmetic Operator

--- prac.py
+++ prac.py
@@ -1,9 +1,5 @@
-#STRING METHODS
-food = 'Eat to Live dont Live to Eat'
+#ARITHMETIC OPERATIONS
 
-print(len(food)) #len() - Count the numbers of characters in a string 
-print(food.upper()) #upper() - Convert a string to upper case
-print(food.lower()) #lower() - Convert a string to lower case
-print(food.find('Eat')) #find() - returns the index of a character in string
-print(food.replace('Eat', 'Munch')) #replace() - replace charaters and words in a string
-print('Eat' in food) #Boolean expression
+i = 50
+i -= 10
+print(i) # Augmented assignmet operator
