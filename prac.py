361f38961d5c5f9218ad222017627d62534f665b
georgegathu <ggeorge.gathu@gmail.com>
2022-12-29 21:42:09 +0300

    Property downpayment app using if statement

--- prac.py
+++ prac.py
@@ -1,5 +1,20 @@
-#ARITHMETIC OPERATIONS
+#IF STATEMENTS
 
-i = 50
-i -= 10
-print(i) # Augmented assignmet operator
+#Write a program:
+# if good credit
+#   put down 10% 
+# otherwise 
+#   put down 20% 
+# print the down payment
+
+property_price = 100000
+good_credit = False
+
+if good_credit:
+    down_payment = 0.1 * property_price
+else:
+    down_payment = 0.2 * property_price
+print(f"Down payment: ksh{down_payment}")
+
+
+
