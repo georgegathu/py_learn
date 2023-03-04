21688666891ad8626ed16e816ade0e29cac37e1a
georgegathu <ggeorge.gathu@gmail.com>
2023-03-04 13:48:30 +0300

    A program to find the Area and Circumference Of a Rectangle

--- rectangle_area.py
+++ rectangle_area.py
@@ -1,6 +1,8 @@
-# A program that prompts users for inputs to find the area of a rectangle 
+# A program that prompts users for inputs to find the area and circumfrence of a rectangle 
 width = float(input("Input Width: ")) 
 height = float(input("Input Height: "))
 area = width * height
 
-print(f"The area of the rectangle is: {area}")
+print(f"The area of the rectangle is {area}")
+circumference = 2 * (width + height)
+print(f"The circumference is {circumference}")
