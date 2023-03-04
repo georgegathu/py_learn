44bf48ff2d696bf8aceadea8b6092214fe20d370
georgegathu <ggeorge.gathu@gmail.com>
2023-03-04 11:59:31 +0300

    CPU and RAM Task Manager App update

--- task_manager.py
+++ task_manager.py
@@ -3,5 +3,6 @@ import psutil
 
 while True:
     print("Your CPU Usage is: " + str(psutil.cpu_percent(1))) # Updates the CPU usage after every 1 second
+    print("Your RAM Usage is: " + str(psutil.virtual_memory()[3]/1000000000)) # This prints the amount of RAM usage
 
 # NB: Use windows command = "Control + C" to escape the program's loop.
