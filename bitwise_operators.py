d8db51c89afa855e47d6be0005ca9349ae715a8b
georgegathu <ggeorge.gathu@gmail.com>
2023-06-07 12:16:17 +0300

    Bitwise - Operators

--- bitwise_operators.py
+++ bitwise_operators.py
@@ -9,19 +9,25 @@ They include:
     >> = RIGHT SHIFT
 """
 # Example 1 (Using & AND Operator)
-x = 4
-y = 5
+x = 4 # 0101 (Binary)
+y = 5 # 0100 (Binary)
 
 print(x & y) # Ans: 4
 
 # Example 2 (Using | OR Operator)
-x = 4
-y = 5
+x = 4 # 0101 (Binary)
+y = 5 # 0100 (Binary)
 
 print(x | y) # Ans: 5
 
 # Example 3 (Using ^ XOR Operator)
-x = 4
-y = 5
+x = 4 # 0101 (Binary)
+y = 5 # 0100 (Binary)
 
 print(x ^ y) # Ans: 1
+
+# Example 4 (Using ~ XOR Operator)
+x = 4 # 0101 (Binary)
+y = 5 # 0100 (Binary)
+
+print(~ y) # Ans: -6
