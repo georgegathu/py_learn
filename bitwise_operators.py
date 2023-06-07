4627eeadd75c3089b78c43c44df4b2eaec750942
georgegathu <ggeorge.gathu@gmail.com>
2023-06-07 11:53:36 +0300

    Bitwise - Operators

---
+++ bitwise_operators.py
@@ -0,0 +1,14 @@
+"""
+Works on bits (binary numbers)
+They include:
+    & = bitwise AND
+    | = bitwise OR
+    ^ = XOR
+    ~ = NOT (COMPLIMENT)
+    << = LEFT SHIFT
+    >> = RIGHT SHIFT
+"""
+x = 4
+y = 5
+
+print(x & y) # Ans: 4
