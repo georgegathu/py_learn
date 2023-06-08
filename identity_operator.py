453ea0b39ad819f08f063499dc475f00dfdeca0b
georgegathu <ggeorge.gathu@gmail.com>
2023-06-08 22:10:39 +0300

    Identity - Operators

---
+++ identity_operator.py
@@ -0,0 +1,13 @@
+"""
+# Identity Operators
+This includes:
+    a) is    (This returns True if both variables are the same object)
+    b) isnot (This returns True if both variables are not the same object)
+They are used to compare objects: If they are the same or not and they share same memory location
+"""
+
+# Example 1 (is Operator)
+x = 5
+y = 5
+
+print(x is y)
