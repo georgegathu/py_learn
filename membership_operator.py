d06df61de08a2226fd0a5b3fab1b662f15b2ee96
georgegathu <ggeorge.gathu@gmail.com>
2023-06-11 00:02:13 +0300

    Membership - Operators

--- membership_operator.py
+++ membership_operator.py
@@ -1,5 +1,12 @@
+# Example 1
 str = 'Bowling'
 
 print('w' in str) # Ans = True
 print('W' in str) # Ans = False
 print('W' not in str) # Ans = True
+
+# Example 2
+x = [10, 20, 30, 40, 50]
+
+print(10 in x) # Ans = True
+print(15 in x) # Ans = False
