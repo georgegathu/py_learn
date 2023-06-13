970c9a20b22d2de03f6c86055ff863feda1448aa
georgegathu <ggeorge.gathu@gmail.com>
2023-06-13 22:32:25 +0300

    f-string formating In Python

---
+++ f-strings.py
@@ -0,0 +1,7 @@
+#f-string formating
+name = "Nairobi"
+age = 30
+height = 11.6
+
+print(f"My name is {name}. I am {age} years old. My height is {height} in meters")
+print(f"Nairobi's mum is {age*2} years old")
