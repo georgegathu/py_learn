af2458b61f7ce140c9d31a7894fe0cb9c30be1f5
georgegathu <ggeorge.gathu@gmail.com>
2023-06-13 22:09:35 +0300

    Functions In Python

--- functions.py
+++ functions.py
@@ -11,3 +11,7 @@ print(round(674, 2)) # Ans = 674
 print(round(674, -1)) # Ans = 670
 print(round(674, -2)) # Ans = 700
 print(round(674, -3)) # Ans = 100 
+
+#Test 2
+print(round(665, -1)) #660
+print(round(675, -1)) #680
