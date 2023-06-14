628f74da8219a9352a6bfd9fb65e2d19619e8422
georgegathu <ggeorge.gathu@gmail.com>
2023-06-14 22:21:43 +0300

    Token Cheecker program using If-else

---
+++ if_if-else.py
@@ -0,0 +1,16 @@
+"""
+Conditional Statement (if and if-else)
+
+#Syntax:
+if condition
+    do thid
+else
+    do this
+"""
+# Example 1: Write a Token monitoring program with conditions
+balance = int(input("Enter balance: "))
+
+if(balance < 2):
+    print("Buy token!")
+else:
+    print("Hurry!! No Token required")
