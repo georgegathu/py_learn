585c131bef8fb950ca93ce37585f0157a7e3cd28
georgegathu <ggeorge.gathu@gmail.com>
2023-06-14 23:13:27 +0300

    nested-elif-else program: to cheeck the height and age of a participant before entry

--- nested_elif.py
+++ nested_elif.py
@@ -13,3 +13,18 @@ if height >= 2:
 else:
     print("Can't ride")
 print("Bye bye!")
+
+# Example 2: Print out numbers 1-4 else display error
+number = int(input("Enter number: "))
+
+if number == 1:
+    print("One")
+elif number == 2:
+    print("Two")
+elif number == 3:
+    print("Three")
+elif number == 4:
+    print("Four")
+else:
+    print("Wrong Number!")
+print("Bye")
