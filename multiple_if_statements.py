9f0c915aaac6f4ee580578f7d50219ff01b5ec11
georgegathu <ggeorge.gathu@gmail.com>
2023-06-18 23:35:56 +0300

    A program to determine if a number is Positive or negative

---
+++ multiple_if_statements.py
@@ -0,0 +1,13 @@
+# A program to define if a number is Positive or negative:
+def multiple_if_statements():
+    number = 10
+
+    if number > 0:
+        print("The number is positive.")
+    elif number == 0:
+        print("The number is zero.")
+    else:
+        print("The number is negative.")
+
+if __name__ == "__main__":
+    multiple_if_statements()
