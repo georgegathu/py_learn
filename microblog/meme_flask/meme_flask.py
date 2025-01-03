921c6a5df2b07b49ae50170ee2a78f5df566b924
georgegathu <ggeorge.gathu@gmail.com>
2025-01-03 14:46:39 +0300

    Variable naming rules in Python

---
+++ microblog/meme_flask/meme_flask.py
@@ -0,0 +1,9 @@
+#!/bin/python
+
+from flask import Flask, render_template
+import requests
+import json
+
+def get_meme():
+    
+
---
+++ var_rules.py
@@ -0,0 +1,18 @@
+# Variable Names
+"""
+You can name a variable anything as long as it obeys the following 
+three rules:
+ •	It can be only one word with no spaces.
+ •	It can use only letters, numbers, and the underscore (_) character.
+ •	It can’t begin with a number.
+ Variables are case sensitive: car, Car, CAR and CaR are four different variables.
+ 
+ --Valid--                              --invalid--
+ current_balance                    current-balance (hyphens are not alloewd)
+ currentBalance                     current balance (spaces are not allowed)
+ account44                          44account (can't begin with a number)
+ _69                                69 (can't begin with a number)
+ TOTAL_SUM                          TOTAL_$UM (special characters like $ are not allowed)
+ bid                                'bid' (special characters like ' are not allowed)
+
+"""
