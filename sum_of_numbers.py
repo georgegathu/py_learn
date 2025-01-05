f976ae5c2d315dc8c538ad0f1b7e17441f64b8b1
georgegathu <ggeorge.gathu@gmail.com>
2025-01-05 21:21:12 +0300

    Sum of 2 numbers

---
+++ sum_of_numbers.py
@@ -0,0 +1,10 @@
+# Python program that prompts the user to enter two numbers, calculates their sum, and then displays the result.
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate sum
+sum = num1 + num2
+
+# Display result
+print(f"The sum is {sum}")
