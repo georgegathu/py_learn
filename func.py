1565885ad680ce2447a4db50ddfed5027bde1e15
georgegathu <ggeorge.gathu@gmail.com>
2025-02-01 21:26:11 +0300

    f-string

--- func.py
+++ func.py
@@ -1,5 +1,8 @@
 # Functions
 '''
 official documentation:
-print(*objects, sep='', end='\n', file=sys.stdout, flush=False)
+print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
 '''
+name = input("Whats your name? ")
+
+print(f"Hello, {name}")
