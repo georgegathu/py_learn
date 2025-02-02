b366d0b8716b9dfc60bbfb72fda19189a62dc910
georgegathu <ggeorge.gathu@gmail.com>
2025-02-02 14:59:34 +0300

    f-string

--- func.py
+++ func.py
@@ -3,6 +3,12 @@
 official documentation:
 print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
 '''
+
+# Ask for user's name
 name = input("Whats your name? ")
 
+# Remove whiespace from str
+name = name.strip()
+
+# Great the user
 print(f"Hello, {name}")
