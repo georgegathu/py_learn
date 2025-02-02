25a0d26dbd2964e98df7fc10a6aae58f25e6a76f
georgegathu <ggeorge.gathu@gmail.com>
2025-02-02 15:09:12 +0300

    str meathods

--- func.py
+++ func.py
@@ -7,8 +7,8 @@ print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
 # Ask for user's name
 name = input("Whats your name? ")
 
-# Remove whiespace from str
-name = name.strip()
+# Remove whiespace from str and CAPITALIZE user's name
+name = name.strip().title()
 
 # Great the user
 print(f"Hello, {name}")
