fe22690ba4f01c126c29286fc9867e8708ee1a79
georgegathu <ggeorge.gathu@gmail.com>
2025-02-03 11:14:52 +0300

    numeric formating

--- float.py
+++ float.py
@@ -3,6 +3,6 @@
 x = float(input("Enter first number: "))
 y = float(input("Enter second number: "))
 
-z = round(x + y) # round off the float number to the nearest integer
+z = x / y
 
-print(f"{z:,}")
+print(f"{z:.2f}")
